3459787c88f24305a666c7b1d6ecd7524c2628f9
Afshal Warsi <afshal.warsi@shottech.co>
2022-04-06 12:39:50 -0700

    Tokenomics complete

--- src/Components/NonAuth/HomePage/HomeSection08.js
+++ src/Components/NonAuth/HomePage/HomeSection08.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import tokenomicsImg01 from '../../../Assets/Home/tokenomicsImg.png'
-import tokenomicsImg02 from '../../../Assets/Home/tokenomicsImgText.png'
 
 const HomeSection08 = () => {
     return (
@@ -18,8 +17,22 @@ const HomeSection08 = () => {
                             <img className='tokenomicsImg01' src={tokenomicsImg01} alt='tokenomicsImg' />
                         </div>
                     </Col>
-                    <Col lg={6}>
-                        <h2 className="mb-0 mt-5">Tokenomics</h2>
+                    <Col lg={6} className='m-auto text-light'>
+                        <div className="features">
+                            <h2 className="text-success">Token Features</h2>
+                            <h5 className='text-secondary'>Passive Income</h5>
+                            <p>8% buy and sale tax redistributed to Arena Token holders in the form of BUSD Daily!</p>
+                            <h5 className='text-secondary'>Deflationary Currency</h5>
+                            <p>Constant burning of tokens until there are only 50 billion of the 75 billion tokens in circulation.</p>
+                            <p>Frequent manual and automatic token buybacks </p>
+                            <h5 className='text-secondary'>Anti-Manipulation Measurements</h5>
+                            <p className='mb-5'>
+                                The Arena Token Contract is coded in a way that prevents big wallets (whales) as well as High Frequency Trading bots and algorithms from manipulating the price to profit from Arena Token holders.
+                            </p>
+                            <p>
+                                Team holds less than 5% of the tokens which are locked and vested in a 5 year period. Most token allocation is towards the development and betterment of the game as well as player rewards.
+                            </p>
+                        </div>
                     </Col>
                 </Row>
             </Container>
